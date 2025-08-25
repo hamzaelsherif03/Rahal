019e4840cb0a5431878f7d54557e4b03fdb8029c
hamzaelsherif03 <hamza.a.elsherif@gmail.com>
2025-08-25 20:52:48 +0300

    k

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,11 +78,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${dmSerifDisplay.variable} ${dmSans.variable} antialiased`}>
       <head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/rahal-logo.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/rahal-logo.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/rahal-logo.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/rahal-logo.png" />
         <meta name="theme-color" content="#1f2937" />
         <meta name="msapplication-TileColor" content="#1f2937" />
       </head>
